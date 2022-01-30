1eb88bcc0c2a77a1db391cacd851fa7fbe0be6e2
Kirill Ateev <kirill.ateev59@gmail.com>
2022-01-30 14:26:39 +0300

    completed EGOR-KURIT nft

---
+++ scripts/mint-nft.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const API_URL = process.env.API_URL;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const alchemyWeb3 = createAlchemyWeb3(API_URL);
+const contract = require("../artifacts/contracts/EGOR-KURIT.sol/TorNFT.json");
+
+const contractAddress = "0x3638883bad83cbbc11D67864C497E1585531a65C";
+const nftContract = new alchemyWeb3.eth.Contract(contract.abi, contractAddress);
+
+const METAMASK_PUBLIC_KEY = process.env.METAMASK_PUBLIC_KEY;
+const METAMASK_PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY;
+
+async function mintNFT(tokenURI) {
+    // get the nonce - nonce is needed for security reasons. It keeps track of the number of
+    // transactions sent from our address and prevents replay attacks.
+  const nonce = await alchemyWeb3.eth.getTransactionCount(METAMASK_PUBLIC_KEY, 'latest');
+  const tx = {
+    from: METAMASK_PUBLIC_KEY, // our MetaMask public key
+    to: contractAddress, // the smart contract address we want to interact with
+    nonce: nonce, // nonce with the no of transactions from our account
+    gas: 1000000, // fee estimate to complete the transaction
+    data: nftContract.methods
+    // the wallet address passed here should be the wallet address of the NFT’s recipient.
+      .createNFT("0x1cde480B90649B839852F5A870B38ABD1D647422", tokenURI)
+      .encodeABI(), // call the createNFT function from our EGOR-KURIT.sol file and pass the account that should receive the minted NFT.
+  };
+
+  const signPromise = alchemyWeb3.eth.accounts.signTransaction(
+    tx,
+    METAMASK_PRIVATE_KEY
+  );
+  signPromise
+    .then((signedTx) => {
+      alchemyWeb3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of our transaction is: ",
+              hash,
+              "\nCheck Alchemy's Mempool to view the status of our transaction!"
+            );
+          } else {
+            console.log(
+              "Something went wrong when submitting our transaction:",
+              err
+            );
+          }
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(" Promise failed:", err);
+    });
+}
+
+mintNFT("https://ipfs.io/ipfs/QmZ3vwAq9ASTcJxFrgzCewyjrPEjmSPyddGnoM1FUkJ5Bn") // pass the CID to the JSON file uploaded to Pinata
